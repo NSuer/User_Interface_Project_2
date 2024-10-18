fe73d4c8f9ae81d05e9711058051db10d6959565
William Braun <braunwb@mail.uc.edu>
2024-10-18 14:11:10 -0400

    added info controls

---
+++ src/lib/InfoControls.svelte
@@ -0,0 +1,56 @@
+@ -0,0 +1,55 @@
+<script>
+   let showControlInfo = false;
+
+   function toggleControlInfo() {
+      showControlInfo = !showControlInfo;
+   }
+ </script>
+
+
+<div class="info-controls-container">
+   <button class="info-controls-button" on:click={toggleControlInfo}>
+      {showControlInfo ? 'Hide Info' : 'Show Info'}
+   </button>
+
+   {#if showControlInfo}
+      <div class="info-controls-box">
+         <p>This control panel simulates voice controls.</p>
+      </div>
+   {/if}
+</div>
+ 
+ <style>
+   
+   .info-controls-container {
+      display: flex;
+      align-items: flex-start;
+      margin-top: 1rem;
+      width: 100%;
+   }
+
+   .info-controls-button {
+      margin-top: 0.5rem;
+      padding: 0.5rem;
+      background-color: rgb(251, 203, 51);   
+      color: black;
+      border: none;
+      border-radius: 10px;
+      cursor: pointer;
+      font-size: 1rem;
+      width: 20%;
+   }
+
+   .info-controls-box {
+      background-color: #f8f9fa;     
+      padding: 0.25rem;                 
+      border: 0.1rem solid #ddd;     
+      margin-bottom: 1.5rem;         
+      border-radius: 0.5rem;        
+      font-size: 1rem;               
+      max-width: 90%; 
+      margin-left: 0.5rem;              
+   }
+
+   
+ </style>
