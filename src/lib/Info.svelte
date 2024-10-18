0cf13fd4d4244e649b2e27d7c34cf577e579ebcf
D3adlyBruh <natesuer@gmail.com>
2024-10-18 14:10:04 -0400

    Update Info.svelte

--- src/lib/Info.svelte
+++ src/lib/Info.svelte
@@ -1,24 +1,28 @@
 <script>
    let showInfo = false;
- 
+
    function toggleInfo() {
-     showInfo = !showInfo;
+      showInfo = !showInfo;
    }
- </script>
- 
- <div class="info-container">
+</script>
+
+<div class="info-container" style="flex-direction: column;">
    <button class="info-button" on:click={toggleInfo}>
-      {showInfo ? 'Hide Info' : 'Show Info'}
+      {showInfo ? "Hide Info" : "Show Info"}
    </button>
-   
+
    {#if showInfo}
       <div class="info-box">
-      <p>This floor plan allows you to add, edit, or remove lights. Click on an empty space to add a new light. Click on an existing light to modify or delete it.</p>
+         <p>
+            This floor plan allows you to add, edit, or remove lights. Click on
+            an empty space to add a new light. Click on an existing light to
+            modify or delete it.
+         </p>
       </div>
    {/if}
 </div>
- 
- <style>
+
+<style>
    .info-container {
       display: flex;
       align-items: flex-start;
@@ -29,27 +33,27 @@
    .info-button {
       position: relative;
 
-      margin-top: 0.5rem;     
-      margin-left: 15px;    
-      padding: 0.5rem;         
-      background-color: rgb(251, 203, 51);   
-      color: black;                 
-      border: none;                  
-      border-radius: 10px;         
-      cursor: pointer;               
-      font-size: 1rem;              
-      width: 20%;       
-      bottom: 7px;             
+      margin-top: 0.5rem;
+      margin-left: 15px;
+      padding: 0.5rem;
+      background-color: rgb(251, 203, 51);
+      color: black;
+      border: none;
+      border-radius: 10px;
+      cursor: pointer;
+      font-size: 1rem;
+      width: 100%;
+      bottom: 7px;
    }
- 
+
    .info-box {
-      background-color: #f8f9fa;     
-      padding: 0.25rem;                 
-      border: 0.1rem solid #ddd;     
-      margin-bottom: 1.5rem;         
-      border-radius: 0.5rem;        
-      font-size: 1rem;               
-      max-width: 90%; 
-      margin-left: 0.5rem;              
+      background-color: #f8f9fa;
+      padding: 0.25rem;
+      border: 0.1rem solid #ddd;
+      margin-bottom: 1.5rem;
+      border-radius: 0.5rem;
+      font-size: 1rem;
+      max-width: 90%;
+      margin-left: 0.5rem;
    }
- </style>
+</style>
